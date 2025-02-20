b043e0ca8eef0f549a73f078a135d7d53824825d
ilyes chaabani <ilyes chaaban857@gmail.com>
2025-02-20 11:26:20 +0100

    .

---
+++ BACKEND/PiDev-BackEnd/ReclamationMicroService/src/test/java/tn/esprit/reclamationmicroservice/ReclamationMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.reclamationmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReclamationMicroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
