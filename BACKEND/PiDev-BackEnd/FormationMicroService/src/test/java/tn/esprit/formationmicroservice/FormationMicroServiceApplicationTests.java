169e070dc37a4a987619574305e9f18d39d129c7
ilyes chaabani <ilyes chaaban857@gmail.com>
2025-02-20 12:08:29 +0100

    MicroServices

---
+++ BACKEND/PiDev-BackEnd/FormationMicroService/src/test/java/tn/esprit/formationmicroservice/FormationMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.formationmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormationMicroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ BACKEND/PiDev-BackEnd/ForumMicroService/src/test/java/tn/esprit/forummicroservice/ForumMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.forummicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumMicroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
