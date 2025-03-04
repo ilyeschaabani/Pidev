014a96ad6b3b2f913f4cd7aeb28e47f59ced8d48
Amiralaffet <101209471+Amiralaffet@users.noreply.github.com>
2025-03-04 12:03:27 +0100

    ajout

--- FRONTEND/src/app/ressource/add-ressource/add-ressource.component.ts
+++ FRONTEND/src/app/ressource/add-ressource/add-ressource.component.ts
@@ -9,47 +9,5 @@ import { RessourceService } from 'src/app/ServicesRessource/ressource-service.se
   styleUrls: ['./add-ressource.component.css']
 })
 export class AddRessourceComponent {
-  ressource: Ressource = {
-    idRessource: '',
-    titre: '',
-    description: '',
-    type: TypeRessource.DOCUMENT,
-    date: new Date(),
-    category: CategoryRessource.OTHER
-  };
-  types = Object.values(TypeRessource);
-  categories = Object.values(CategoryRessource);
-
-  successMessage: string = '';
-  errorMessage: string = '';
-
-  constructor(private http: HttpClient, private ressourceService: RessourceService) {
-    this.resetForm();
-  }
-
-  onSubmit(): void {
-    this.ressourceService.create(this.ressource).subscribe({
-      next: (response: any) => {
-        this.successMessage = 'Ressource ajoutée avec succès';
-        this.resetForm();
-      },
-      error: (error: any) => {
-        this.successMessage = '';
-        this.errorMessage = 'Erreur lors de l\'ajout de la ressource';
-      }
-    });
-  }
-
-  resetForm(): void {
-    this.ressource = { 
-      idRessource: '', 
-      titre: '', 
-      description: '', 
-      type: TypeRessource.DOCUMENT, 
-      date: new Date(), 
-      category: CategoryRessource.OTHER 
-    };
-    this.successMessage = '';
-    this.errorMessage = '';
-  }
+ 
 }
--- FRONTEND/src/app/ressource/ressource-list/ressource-list.component.ts
+++ FRONTEND/src/app/ressource/ressource-list/ressource-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http'; // Assurez-vous d'importer HttpClient
 import { RessourceService } from 'src/app/ServicesRessource/ressource-service.service';
 import { Ressource, TypeRessource, CategoryRessource } from 'src/app/models/ressource.model';
 
@@ -9,9 +10,21 @@ import { Ressource, TypeRessource, CategoryRessource } from 'src/app/models/ress
 })
 export class RessourceListComponent implements OnInit {
   ressources: Ressource[] = [];
-  errorMessage = '';
+  ressource: Ressource = {
+    idRessource: '',
+    titre: '',
+    description: '',
+    type: TypeRessource.DOCUMENT, // Assurez-vous que ce type est défini dans votre modèle
+    date: new Date(),
+    category: CategoryRessource.OTHER // Assurez-vous que cette catégorie est définie
+  };
+  types = Object.values(TypeRessource);
+  categories = Object.values(CategoryRessource);
+  
+  successMessage: string = '';
+  errorMessage: string = '';
 
-  constructor(private service: RessourceService) {}
+  constructor(private service: RessourceService, private http: HttpClient) {}
 
   ngOnInit(): void {
     this.loadData();
@@ -25,7 +38,7 @@ export class RessourceListComponent implements OnInit {
   }
 
   deleteRessource(id: string): void {
-    if(confirm('Confirmer suppression ?')) {
+    if (confirm('Confirmer suppression ?')) {
       this.service.delete(id).subscribe({
         next: () => {
           this.ressources = this.ressources.filter(r => r.idRessource !== id);
@@ -33,5 +46,32 @@ export class RessourceListComponent implements OnInit {
         error: (err) => this.errorMessage = 'Erreur suppression: ' + err.message
       });
     }
+  }
+
+  onSubmit(): void {
+    this.service.create(this.ressource).subscribe({
+      next: () => {
+        this.successMessage = 'Ressource ajoutée avec succès';
+        this.resetForm();
+        this.loadData(); // Recharge la liste des ressources après ajout
+      },
+      error: () => {
+        this.successMessage = '';
+        this.errorMessage = 'Erreur lors de l\'ajout de la ressource';
+      }
+    });
+  }
+
+  resetForm(): void {
+    this.ressource = { 
+      idRessource: '', 
+      titre: '', 
+      description: '', 
+      type: TypeRessource.DOCUMENT, 
+      date: new Date(), 
+      category: CategoryRessource.OTHER 
+    };
+    this.successMessage = '';
+    this.errorMessage = '';
   }
 }
